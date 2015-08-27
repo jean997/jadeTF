86dc1e64347db62ed818010e0072e22167af0284
Jean Morrison <jeanm5@uw.edu>
2015-08-27 16:39:05 -0700

    Update options for sd calculation

--- R/bs_var_tf.R
+++ R/bs_var_tf.R
@@ -16,50 +16,57 @@
 #'   \item{\code{var}}{Estimated variance.}
 #' }
 #'@export
-bs_var_tf <- function(Y, lambda, sample.size,
-										READS=NULL,  sds=NULL, calc.sds=TRUE,
+bs_var_tf <- function(Y, lambda,
+										sds=NULL,
+										READS=NULL, sd.type=c("Equal", "Binomial", "Poisson"), calc.sds=TRUE,
 										pos=NULL, scale.pos=NULL, ord=2,
 										n.rep=100){
 
-		p <- dim(Y)[1] #Number of sites
-		k <- dim(Y)[2] #Number of samples
+  sd.type <- match.arg(sd.type)
+  if(sd.type == "Binomial" & is.null(READS)){
+    stop("For Binomial type please provide READS")
+  }
 
-		#Scale positions
-    if(!is.null(pos)){
-      stopifnot(length(pos)==p)
-    }else{
-        pos <- 1:p
-    }
-		pos.give=pos
-    if(!is.null(scale.pos)){
-      R <-range(pos)
-      pos <- scale.pos* ((pos-R[1])/(R[2]-R[1]))
-    }
+	p <- dim(Y)[1] #Number of sites
+	k <- dim(Y)[2] #Number of samples
 
-		if(is.null(sds)) sds <- rep(1, p)
-		all.fits <- matrix(0, p, n.rep)
-		i <- 1
-		while(i <= n.rep){
-			cat(i, "..")
-			S <- sample(1:k, size=k, replace=TRUE)
-			new.Y <- Y[, S]
-			if(!is.null(READS)){
-				new.READS <- READS[, S]
-				y= rowSums(new.Y)/rowSums(new.READS)
-				if(calc.sds){
-				  yhat <- (rowSums(new.Y)+0.5)/(rowSums(new.READS)+1)
-				  new.sds <- sqrt(yhat*(1-yhat)/rowSums(new.READS))
-				}else{
-				  new.sds <- sds
-				}
-			}else{
-				y <- rowMeans(new.Y)
-				new.sds <- sds
-			}
-			f <- fit_one(y, lambda, pos, new.sds, sample.size, ord=ord)
-			all.fits[,i] <- f$fit
-			i <- i+1
+	#Scale positions
+  if(!is.null(pos)){
+    stopifnot(length(pos)==p)
+  }else{
+    pos <- 1:p
+  }
+	pos.given=pos
+  if(!is.null(scale.pos)){
+    R <-range(pos)
+    pos <- scale.pos* ((pos-R[1])/(R[2]-R[1]))
+  }
+
+	if(is.null(sds)) sds <- rep(1, p)
+	all.fits <- matrix(0, p, n.rep)
+	i <- 1
+	while(i <= n.rep){
+		cat(i, "..")
+		S <- sample(1:k, size=k, replace=TRUE)
+		new.Y <- Y[, S]
+		if(sd.type=="Binomial"){
+		  new.READS <- READS[, S]
+		  r <- binom.func(new.Y, new.READS)
+		  y <- r$y
+		  new.sds <- r$sds
+		}else if(sd.type=="Poisson"){
+		  r <- poisson.func(new.Y)
+		  y<- r$y
+		  new.sds <- r$sds
+		}else if(sd.type=="Equal"){
+		  y <- rowSums(new.Y)/k
+		  new.sds <- rep(1, p)
 		}
+		if(!calc.sds) new.sds <- sds
+		f <- fit_one(y, lambda, pos, new.sds, sample.size, ord=ord)
+		all.fits[,i] <- f$fit
+		i <- i+1
+	}
 		cat("\n")
 		avg.fit <- rowMeans(all.fits)
 		var.fit <- (1/(n.rep-1))*rowSums((all.fits-avg.fit)^2)
