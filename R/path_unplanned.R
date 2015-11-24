3806e5f5b29fbe275abc1b6bb1b21b985a79f91a
Jean Morrison <jeanm5@uw.edu>
2015-11-24 11:20:34 -0800

    Change thresh for non converged fits

--- R/path_unplanned.R
+++ R/path_unplanned.R
@@ -153,7 +153,7 @@ jade_path <- function(n.fits, out.file, fit0 = NULL, temp.file=NULL,
 		                " l1.total: ", l1.total[i], "\n")
 		converged[i] <- fit$converged
     #If not converged - backtrack until you are close enough to conver (experimental)
-		if(!converged[i] & (log.gammas[closest.idx]-log.gammas[i]) >= 4*buffer){
+		if(!converged[i] & (log.gammas[closest.idx]-log.gammas[i]) >= 2*buffer){
 		  new.gamma <- log.gammas[closest.idx] + (log.gammas[closest.idx]-log.gammas[i])/2
 		  cat("Backtracking: ", new.gamma)
 		  next
