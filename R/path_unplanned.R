256af0b91ba7dc965b80eac57e28eb01567e6a7b
Jean Morrison <jeanm5@uw.edu>
2016-04-07 11:14:51 -0700

    change backtracking conditions

--- R/path_unplanned.R
+++ R/path_unplanned.R
@@ -164,7 +164,7 @@ jade_path <- function(n.fits, out.file, fit0 = NULL, temp.file=NULL,
 		                " l1.total: ", l1.total[i], "\n")
 		converged[i] <- fit$converged
     #If not converged - go back until you are close enough to converge (experimental)
-		if(!converged[i] & abs(log.gammas[closest.idx]-log.gammas[i]) >= 2*buffer & i > 6 ){
+		if(!converged[i] & abs(log.gammas[closest.idx]-log.gammas[i]) >= 2*buffer & i > 6 & is.finite(log.gammas[closest.idx])){
 		  new.gamma <- min(log.gammas[closest.idx], log.gammas[i]) + abs(log.gammas[closest.idx]-log.gammas[i])/2
 		  cat("Backtracking: ", new.gamma, "\n")
 		  #while(min(abs(new.gamma-log.gammas[-1])) < buffer) new.gamma <- new.gamma + buffer
